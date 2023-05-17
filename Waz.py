f14d8064a5ab0c2b7531ec84c04aa5e5bb1954da
EloToJa <elotoja@protonmail.com>
2023-05-17 19:04:32 +0200

    aktualizacja

--- Waz.py
+++ Waz.py
@@ -1,5 +1,8 @@
 import pygame
 from Kierunek import Kierunek
+from Segment import Segment
+import copy
+from typing import List
 
 
 class Waz(pygame.sprite.Sprite):
@@ -10,6 +13,10 @@ class Waz(pygame.sprite.Sprite):
 
         self.kierunek = Kierunek.GORA
         self.nowy_kierunek = Kierunek.GORA
+
+        self.ostatnia_pozycja = self.pozycja
+        self.dodaj_segment = False
+        self.segmenty: List[Segment] = []
 
     def zmien_kierunek(self, kierunek: Kierunek):
         zmiana_mozliwa = True
@@ -32,6 +39,8 @@ class Waz(pygame.sprite.Sprite):
             self.oryginalny_obraz, self.kierunek.value * -90
         )
 
+        self.ostatnia_pozycja = copy.deepcopy(self.pozycja)
+
         if self.kierunek == Kierunek.GORA:
             self.pozycja.move_ip(0, -32)
         if self.kierunek == Kierunek.PRAWO:
@@ -40,3 +49,30 @@ class Waz(pygame.sprite.Sprite):
             self.pozycja.move_ip(-32, 0)
         if self.kierunek == Kierunek.DOL:
             self.pozycja.move_ip(0, 32)
+
+        for i in range(len(self.segmenty)):
+            if i == 0:
+                self.segmenty[i].przesun(self.ostatnia_pozycja)
+            else:
+                self.segmenty[i].przesun(self.segmenty[i - 1].ostatnia_pozycja)
+
+        if self.dodaj_segment:
+            nowy_segment = Segment()
+
+            nowa_pozycja = None
+            if len(self.segmenty) > 0:
+                nowa_pozycja = copy.deepcopy(self.segmenty[-1].pozycja)
+            else:
+                nowa_pozycja = copy.deepcopy(self.ostatnia_pozycja)
+
+            nowy_segment.pozycja = nowa_pozycja  #!
+
+            self.segmenty.append(nowy_segment)
+            self.dodaj_segment = False
+
+    def rysuj_segmenty(self, ekran: pygame.Surface):
+        for segment in self.segmenty:
+            ekran.blit(segment.obraz, segment.pozycja)
+
+    def jedz_jablko(self):
+        self.dodaj_segment = True
